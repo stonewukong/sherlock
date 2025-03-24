cac9fe25141b88a1c15ea3ed3c7040be9177e9ef
stonewukong <marnitanmayadithya@gmail.com>
2025-03-24 15:41:51 +0530

    add tailwind

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss() as any],
+  }),
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   runner: {
